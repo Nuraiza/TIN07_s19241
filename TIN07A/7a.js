b1da262e7df8b478f0eec775d1aa4d97f291fdcb
Nuraiza <57836964+Nuraiza@users.noreply.github.com>
2020-12-21 10:34:29 +0100

    Add files via upload

---
+++ TIN07A/7a.js
@@ -0,0 +1,68 @@
+//http://localhost:8080/?operation=addition&fnumber=23&snumber=44
+//http://localhost:8080/?operation=substraction&fnumber=23&snumber=12
+//http://localhost:8080/?operation=multiplication&fnumber=11&snumber=12
+//http://localhost:8080/?operation=division&fnumber=24&snumber=6
+
+//http://localhost:8080/?operation=notreal&fnumber=23&snumber=12
+//http://localhost:8080/?operation=addition&fnumber=&snumber=12
+//http://localhost:8080/?operation=addition&fnumber=23&snumber=""
+
+var http = require('http');
+var url = require('url');
+
+
+http.createServer(function (req, res) {
+   
+ var q = url.parse(req.url, true).query;
+
+    if( typeof(q.operation) !== "undefined" && q.operation){
+       if(typeof(q.fnumber)!=="undefined"&& q.fnumber){
+        if(typeof(q.snumber)!=="undefined"&& q.snumber){
+        var result;
+    switch(q.operation){
+        case "addition":
+            result = q.fnumber+q.snumber;
+            var txtAdd = q.fnumber+ " + "+q.snumber +" is "+ result;
+           res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(txtAdd);
+            break;
+        case "substraction":
+            result = q.fnumber-q.snumber;
+            var txtSub = q.fnumber+ " - "+q.snumber +" is "+ result;
+             res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(txtSub);
+            break;
+        case "multiplication":
+            result = q.fnumber*q.snumber;
+            var txtMul = q.fnumber+ " * "+q.snumber +" is "+ result;
+             res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(txtMul);
+            break;
+        case "division":
+            result = q.fnumber/q.snumber;
+            var txtDiv = q.fnumber+ " / "+q.snumber +" is "+ result;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(txtDiv);
+            break; 
+        default :
+           res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end("Operation not found");
+    }         
+        }else{
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end("Operation can not be found");  
+           
+        }        
+    } else{
+          res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end("First number can not be found");
+       
+    }
+       } else{
+                 res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end("Second number can not be found"); 
+        
+                  }
+                 
+   
+}).listen(8080);
